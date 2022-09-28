386b6184e7eec6a3cab8c3e9a9ae86e1a3498ac3
Rasmus Grønbek <cph-rg86@cphbusiness.dk>
2022-09-28 18:36:58 +0200

    Navbar, Footer and Home

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Logo from '../img/Logo.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <img src={Logo} alt="" />
+      <span>Made with ❤️ from the dev team</span>
+
+    </footer>
   )
 }
 
