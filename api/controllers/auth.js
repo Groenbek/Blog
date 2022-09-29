70d3c4feb776e80c2b7a85eaa7e0cecbfc9e768a
Rasmus Grønbek <cph-rg86@cphbusiness.dk>
2022-09-29 16:16:44 +0200

    login logout completed. Cookies wipe after logout

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -29,25 +29,35 @@ export const login = (req, res) => {
 
     // cehck user
 
-    const q = "SELECT * FROM users WHERE username = ?"
-    db.query(q, [req, body, username], (err, data) => {
-        if (err) return res.json(err)
-        if(data.length === 0) return res.sttus(404).json("User not found!")
+    const q = "SELECT * FROM users WHERE username = ?";
 
-        // Check pw
-        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password)
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("User not found!");
 
-        if(!isPasswordCorrect) return res.status(400).json("Wrong username or password!")
-    
-        const token = jwt.sign({id:data[0].id}, "jwtkeyyeeehaw")
-        const {password, ...other} = data[0]
+    //Check password
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
 
-        res.cookie('access_token', token,{
-            httpOnly:true
-        } ).status(200).json(data[0])
-    })
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password!");
 
-}
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  });
+};
 export const logout = (req, res) => {
-
-}
+    res.clearCookie("access_token",{
+      sameSite:"none",
+      secure:true
+    }).status(200).json("User has been logged out.")
+  };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import Logo from '../img/Logo.png'
+import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import Logo from "../img/Logo.png";
 
 const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="container">
         <div className="logo">
           <Link to="/">
-          <img src={Logo} alt="" />
+            <img src={Logo} alt="" />
           </Link>
         </div>
         <div className="links">
@@ -30,21 +34,23 @@ const Navbar = () => {
           <Link className="link" to="/?cat=food">
             <h6>FOOD</h6>
           </Link>
-          <span>
-            John
-          </span>
-          <span>
-            Logout
-          </span>
-          <span className='write'>
-            <Link className='link' to='write'>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to={"/login"}>
+              Login
+            </Link>
+          )}
+          <span className="write">
+            <Link className="link" to="write">
               Write
             </Link>
           </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
